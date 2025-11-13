69c2f5a42125705fd56bd58abfb6e70642c4bb3c
Henry <tongsik98@gmail.com>
2025-11-13 13:51:49 -0800

    refactor: add _blank for a tag so it opens individual window and redirect

--- components/DetailBlocks/DetailTop.js
+++ components/DetailBlocks/DetailTop.js
@@ -16,7 +16,7 @@ export const DetailTop = ({ course }) => {
       <a href="${courseInfo}" style="color: #2B7DA1; target="_blank"">${title}</a>
       <p style="border-left: 2px solid black; padding-left: 8px;">${code}</p>
       <p>Section ${section}</p>
-      <a href="${blackboard}" style="border-left: 2px solid black; padding-left: 8px; color: #2B7DA1;">Blackboard</a>
+      <a href="${blackboard}" target="_blank" style="border-left: 2px solid black; padding-left: 8px; color: #2B7DA1;">Blackboard</a>
     </div>
     <div style="display: flex; align-items: center; font-size: 14px">
       <p>${startDate}</p>
